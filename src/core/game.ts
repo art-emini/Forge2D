b67a7d60417d0169c12546b9ec79029cf8518aea
ksplatdev <aviatorbuzz921@gmail.com>
2023-06-25 14:43:43 -0400

    move renderer to game class

--- src/core/game.ts
+++ src/core/game.ts
@@ -1,11 +1,27 @@
 import Forge2D from '..';
+import BaseRenderer from './renderer/baseRenderer';
 import Scene from './scene/scene';
+import getRendererMethod from '../utils/getRendererMethod';
+import CanvasRenderer from './renderer/canvas/canvasRenderer';
+import WebGLRenderer from './renderer/webgl/webGLRenderer';
+import getCanvas from '../utils/getCanvas';
 
 export default class Game {
   public readonly scenes: Scene[] = [];
   public currentScene?: Scene;
 
+  public readonly renderMethod: Forge2D.Types.Misc.Renderer;
+  public readonly renderer: BaseRenderer;
+
   constructor(public readonly config?: Forge2D.Types.Config) {
     console.log(`Powered by Forge2D MIT LICENSE`);
+
+    this.renderMethod = this.config?.renderer || getRendererMethod();
+
+    if (this.renderMethod === 0) {
+      this.renderer = new CanvasRenderer(getCanvas(this.config?.canvas));
+    } else {
+      this.renderer = new WebGLRenderer(getCanvas(this.config?.canvas));
+    }
   }
 }
--- src/core/scene/scene.ts
+++ src/core/scene/scene.ts
@@ -1,27 +1,11 @@
 import Game from '../game';
-import BaseRenderer from '../renderer/baseRenderer';
+
 import Body from '../body/body';
-import Forge2D from '../..';
-import getRendererMethod from '../../utils/getRendererMethod';
-import CanvasRenderer from '../renderer/canvas/canvasRenderer';
-import WebGLRenderer from '../renderer/webgl/webGLRenderer';
-import getCanvas from '../../utils/getCanvas';
 
 export default class Scene {
-  public readonly renderMethod: Forge2D.Types.Misc.Renderer;
-  public readonly renderer: BaseRenderer;
-
   public readonly bodies: Body[] = [];
 
-  constructor(public readonly game: Game, public readonly name: string) {
-    this.renderMethod = this.game.config?.renderer || getRendererMethod();
-
-    if (this.renderMethod === 0) {
-      this.renderer = new CanvasRenderer(getCanvas(this.game.config?.canvas));
-    } else {
-      this.renderer = new WebGLRenderer(getCanvas(this.game.config?.canvas));
-    }
-  }
+  constructor(public readonly game: Game, public readonly name: string) {}
 
   public async preload() {
     // user implements
